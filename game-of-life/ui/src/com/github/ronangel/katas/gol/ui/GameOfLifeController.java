cc683bb9e53601d8cf09e33c0a012b32d18ead2e
Ron Angel <ron.r.angel@gmail.com>
2017-09-30 08:23:14 -0400

    Added UI module to project

---
+++ game-of-life/ui/src/com/github/ronangel/katas/gol/ui/GameOfLifeController.java
@@ -0,0 +1,7 @@
+package com.github.ronangel.katas.gol.ui;
+
+public class GameOfLifeController
+{
+
+
+}
---
+++ game-of-life/ui/src/com/github/ronangel/katas/gol/ui/GameOfLifeUI.java
@@ -0,0 +1,16 @@
+package com.github.ronangel.katas.gol.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameOfLifeUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
